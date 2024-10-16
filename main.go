bfc55fd87fe99affaa9f89f656fc81f9dd81fad7
Allan <allanfelipedk@gmail.com>
2024-10-16 19:57:53 -0300

    feat: modularizando o projeto e respondendo o json

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"api-go-rest/models"
+	"api-go-rest/routes"
+	"fmt"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	fmt.Println("Iniciando o servidor Rest em Go")
+	routes.HandleRequest()
+}
